f0fbef4eefb2972544644c65dc16e27ca697e1f2
Subithan <161576972+Subithan@users.noreply.github.com>
2024-08-06 09:03:00 +0530

    Add files via upload

---
+++ q3.scala
@@ -0,0 +1,17 @@
+def isPrime(num: Int): Boolean = {
+  num match {
+    case n if n <= 1 => false
+    case 2 => true
+    case n if n % 2 == 0 => false
+    case _ => !(3 to math.sqrt(num).toInt by 2).exists(i => num % i == 0)
+  }
+}
+
+def filterPrime(numbers: List[Int]): List[Int] = {
+  numbers.filter(num => isPrime(num))
+}
+
+def main(args: Array[String]): Unit = {
+  val inputList = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+  println(filterPrime(inputList))
+}
